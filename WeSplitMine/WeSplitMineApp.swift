8a359f8f23d630ec0d3a8aea7082bb3db39bd7f2
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-09-17 17:26:50 +0200

    Initial Commit

---
+++ WeSplitMine/WeSplitMineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitMineApp.swift
+//  WeSplitMine
+//
+//  Created by Natalia Nikiforuk on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitMineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
